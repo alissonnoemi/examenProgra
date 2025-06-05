1d38e29479ff14a330cb2dae5e98370b455db6a2
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-06-05 10:00:50 -0500

    exn

--- src/main/java/com/itsqmet/entity/Restaurante.java
+++ src/main/java/com/itsqmet/entity/Restaurante.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -13,9 +15,13 @@ public class Restaurante {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private long id;
     @Column(unique = true)
+    @Size(min=3, max=30)
     private String nombre;
+    @Size(min = 10, max = 100, message = "La descripción debe tener entre 10 y 100 caracteres")
     private String descripcion;
+    @Pattern(regexp = "^\\d+(\\.\\d{1,2})?$", message = "El precio debe ser un número válido")
     private String precio;
+    @Size(min = 3, max = 30, message = "La categoría debe tener entre 3 y 30 caracteres")
     private String categoria;
     private String disponibilidad;
 
